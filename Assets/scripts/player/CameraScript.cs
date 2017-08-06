5455d6ea3b7f0604958448c8e2d274683d416cbb
DoYeong Han <rbfwmqwntm@naver.com>
2017-08-06 16:50:55 +0900

    Handy Unity3D v1.0.0.0
    - initial commit.
    
    end.

---
+++ Assets/scripts/player/CameraScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <summary>
+/// 
+/// </summary>
+public class CameraScript : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    void Start()
+    {
+        Camera cam = Camera.main;
+
+        cam.aspect = 2;
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/scripts/player/KeyPressInfo.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+
+
+/// <summary>
+/// 
+/// </summary>
+struct KeyPressInfo
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public KeyCode keyCode;
+    /// <summary>
+    /// 
+    /// </summary>
+    public float interval;
+
+
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="keyCode"></param>
+    /// <param name="interval"></param>
+    public KeyPressInfo(KeyCode keyCode, float interval)
+    {
+        this.keyCode = keyCode;
+        this.interval = interval;
+    }
+}
